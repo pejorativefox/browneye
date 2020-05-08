dfafba59da0fcf965a7b41df2301924b6264ac0a
Chris Statzer <chris.statzer@qq.com>
2020-05-08 03:23:16 +0000

    cleanup of iso build

--- scripts/browneye-build-iso.sh
+++ scripts/browneye-build-iso.sh
@@ -1,9 +1,15 @@
 #/bin/bash
 
-ROOT=$1
+ROOT="`pwd`/iso_build"
+SQUASH_ROOT=$ROOT/squash_root
+CD_ROOT=$ROOT/cd_root
 
-pushd $ROOT
+echo $ROOT
+echo $SQUASH_ROOT
+echo $CD_ROOT
 
+mkdir -pv $SQUASH_ROOT
+pushd $SQUASH_ROOT
 mkdir -pv {dev,proc,sys,lib,home,tmp,root,boot}
 
 ln -sv lib lib64
@@ -24,26 +30,27 @@ mkdir -pv var/{opt,cache,lib/{color,misc,locate},local}
 # /run
 mkdir -pv run
 ln -sv ../run var/run
-
 touch run/utmp
-
 mkdir -pv run/lock
 ln -sv ../run/lock var/lock
 
+# yup setup
 mkdir -pv etc/yum.repos.d
 cp /etc/yum.repos.d/core.repo etc/yum.repos.d/
 
 popd
 
-dnf-3 --installroot $ROOT --releasever=1 -y  install \
+dnf-3 --installroot $SQUASH_ROOT --releasever=1 -y  install \
 	bash rpm dnf
 
 
-mkdir -pv $ROOT/isolinux/kernel
-cp /share/syslinux/isolinux.bin $ROOT/isolinux
-cp /share/syslinux/ldlinux.c32 $ROOT/isolinux
-cp /share/syslinux/memdisk $ROOT/isolinux/kernel
+mkdir -pv $CD_ROOT/isolinux/kernel
+cp /share/syslinux/isolinux.bin $CD_ROOT/isolinux
+cp /share/syslinux/ldlinux.c32 $CD_ROOT/isolinux
+cp /share/syslinux/memdisk $CD_ROOT/isolinux/kernel
+
+mksquashfs $SQUASH_ROOT $CD_ROOT/brown.sqsh
 
 genisoimage -o output.iso \
             -b isolinux/isolinux.bin -c isolinux/boot.cat \
-	    -no-emul-boot -boot-load-size 4 -boot-info-table $ROOT
+	    -no-emul-boot -boot-load-size 4 -boot-info-table -debug $CD_ROOT
