dd814ccb8a6d92e6865862a15ac451905bab8687
Chris Statzer <chris.statzer@gmail.com>
2020-05-16 06:06:14 +0000

    iso build script now uses the normal install script.

--- scripts/browneye-build-iso.sh
+++ scripts/browneye-build-iso.sh
@@ -9,41 +9,8 @@ echo $SQUASH_ROOT
 echo $CD_ROOT
 
 mkdir -pv $SQUASH_ROOT
-pushd $SQUASH_ROOT
-mkdir -pv {dev,proc,sys,lib,home,tmp,root,boot}
 
-ln -sv lib lib64
-ln -sv . usr
-
-mknod -m 600 dev/console c 5 1
-mknod -m 666 dev/null c 1 3
-
-# /tmp
-chmod -v 777 tmp
-chmod -v +t tmp
-
-# /var 
-mkdir -pv var
-mkdir -pv var/{log,mail,spool}
-mkdir -pv var/{opt,cache,lib/{color,misc,locate},local}
-
-# /run
-mkdir -pv run
-ln -sv ../run var/run
-touch run/utmp
-mkdir -pv run/lock
-ln -sv ../run/lock var/lock
-
-# yup setup
-mkdir -pv etc/yum.repos.d
-cp /etc/yum.repos.d/core.repo etc/yum.repos.d/
-
-popd
-
-dnf-3 --installroot $SQUASH_ROOT --releasever=1 -y  install \
-	finit bash util-linux coreutils
-
-cp ./iso/finit.conf $SQUASH_ROOT/etc
+./scripts/browneye-install.sh $SQUASH_ROOT
 
 mkdir -pv $CD_ROOT/isolinux/kernel
 cp /share/syslinux/isolinux.bin $CD_ROOT/isolinux
