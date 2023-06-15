322e91fea038805d56ffea917f8521e39f1e7859
Chris Statzer <chris.statzer@gmail.com>
2023-06-15 01:21:43 +0000

    Added some comments to the efi iso build  script

--- scripts/build_iso_efi.sh
+++ scripts/build_iso_efi.sh
@@ -8,10 +8,11 @@ echo $ROOT
 echo $SQUASH_ROOT
 echo $CD_ROOT
 
+# install core os to the squash root
 mkdir -pv $SQUASH_ROOT
-
 ./scripts/install.sh $SQUASH_ROOT
 
+# cheate needed directories in the cd root
 mkdir -pv $CD_ROOT/EFI/BOOT/
 mkdir -pv $CD_ROOT/boot/grub/x86_64-efi
 
@@ -24,25 +25,34 @@ grub-mkimage -o $CD_ROOT/EFI/BOOT/bootx64.efi \
              -O x86_64-efi \
              search iso9660 configfile normal memdisk tar fat
 
+# create vfat EFI raw parition
 dd if=/dev/zero of=$CD_ROOT/EFI/BOOT/efiboot.img bs=512 count=2880
 mkfs.fat -F 12 -n 'EFIBOOTISO' $CD_ROOT/EFI/BOOT/efiboot.img
 
 mmd -i $CD_ROOT/EFI/BOOT/efiboot.img ::EFI
 mmd -i $CD_ROOT/EFI/BOOT/efiboot.img ::EFI/BOOT
 
-
 mcopy -i $CD_ROOT/EFI/BOOT/efiboot.img $CD_ROOT/EFI/BOOT/bootx64.efi ::EFI/BOOT/bootx64.efi
 mcopy -i $CD_ROOT/EFI/BOOT/efiboot.img iso/grub-stub.cfg ::EFI/BOOT/grub-stub.cfg
 
+# create needed direcotries on the cdroot
 mkdir -pv $CD_ROOT/boot
 mkdir -pv $CD_ROOT/LiveOS
+
+# create squashfs
 mksquashfs $SQUASH_ROOT $CD_ROOT/LiveOS/squashfs.img
 
+#create initrd
 dracut --no-hostonly --add "dmsquash-live pollcdrom" $CD_ROOT/boot/initrd.img
 
-cp /boot/vmlinuz-5.6.14 $CD_ROOT/boot/vmlinuz
+# copy kernel and grub config
+KERNEL="$(basename $(ls -1r --sort=version ${SQUASH_ROOT}/boot/vmlinuz* | head -n 1))"
+KERNEL_VER="${KERNEL#vmlinuz-}"
+
+cp /boot/$KERNEL $CD_ROOT/boot/vmlinuz
 cp iso/grub.cfg $CD_ROOT/boot/grub
 
+# build the iso
 xorriso -as mkisofs \
 	-V 'BROWNEYE' \
         -e /EFI/BOOT/efiboot.img \
